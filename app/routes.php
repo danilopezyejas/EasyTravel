8979a0a0a42d76ddfd67a49b5c1c0db5e930f724
danilopezyejas <lopezyejas@gmail.com>
2020-04-16 21:40:27 -0300

    Primer comit

---
+++ app/routes.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+use App\Application\Actions\User\ListUsersAction;
+use App\Application\Actions\User\ViewUserAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+return function (App $app) {
+    $app->get('/', function (Request $request, Response $response) {
+        $loader = new FilesystemLoader(__DIR__ . '/../vistas');
+        $twig = new Environment($loader);
+        $response->getBody()->write($twig->render('index.twig'));
+        return $response;
+    });
+
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+        $group->get('/{id}', ViewUserAction::class);
+    });
+
+    $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+};
