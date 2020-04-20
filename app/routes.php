01df79edd14b829bda96c02424c583cdb61349e6
danilopezyejas <lopezyejas@gmail.com>
2020-04-20 11:08:44 -0300

    prueba

--- app/routes.php
+++ app/routes.php
@@ -20,6 +20,7 @@ return function (App $app) {
 
     $app->group('/users', function (Group $group) {
         $group->get('', ListUsersAction::class);
+        $group->get('/', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
     });
 
